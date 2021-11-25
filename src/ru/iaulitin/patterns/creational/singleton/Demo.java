f654b915b25c6c44d9146efa7dbe4adf731aee10
ulitin.i <ulitin.i@milandr.ru>
2021-11-25 18:57:32 +0300

    Implemented Singleton demo

---
+++ src/ru/iaulitin/patterns/creational/singleton/Demo.java
@@ -0,0 +1,9 @@
+package ru.iaulitin.patterns.creational.singleton;
+
+public class Demo {
+
+    public static void main(String[] args) {
+        MyLovedOne singleton = MyLovedOne.WIFE;
+        singleton.stroll();
+    }
+}
---
+++ src/ru/iaulitin/patterns/creational/singleton/MyLovedOne.java
@@ -0,0 +1,11 @@
+package ru.iaulitin.patterns.creational.singleton;
+
+public enum MyLovedOne {
+    WIFE;
+
+    private String name = "Rita";
+
+    public void stroll() {
+        System.out.println("Let's make a stroll");
+    }
+}
